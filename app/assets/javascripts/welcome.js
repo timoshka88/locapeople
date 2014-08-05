1fa6bed91ca9c02776b223642eddff0781f8a9f3
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-08-05 12:25:59 -0500

    added a controller and view for the landing page

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,7 @@
+// var canvas = document.getElementById('landing-canvas');
+// var context = canvas.getContext('2d');
+// var imageObj = new Image();
+// imageObj.onload = function() {
+//   context.drawImage(imageObj, 69, 50);
+//   };
+// imageObj.src = 'assets/image01.jpg';
