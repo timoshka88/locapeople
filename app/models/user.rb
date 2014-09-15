660eb7839200763c8823ead5bea3c09b645b6664
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-09-15 16:26:12 -0500

    generated model for LookupDrinkType, defined relationship many-to-many on the models user and lookupDrinkType

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 	has_and_belongs_to_many :lookup_cuisine_types
 	has_and_belongs_to_many :lookup_music_types
 	has_and_belongs_to_many :lookup_spirits
+	has_and_belongs_to_many :lookup_drink_types
 	belongs_to 							:lookup_relationship_status
   # attr_accessible :title, :body
 end
---
+++ db/migrate/20140915212348_create_lookup_drink_types.rb
@@ -0,0 +1,10 @@
+class CreateLookupDrinkTypes < ActiveRecord::Migration
+  def change
+    create_table :lookup_drink_types do |t|
+    	t.string 		:name
+    	t.string		:description
+    	
+      t.timestamps
+    end
+  end
+end
