d48d68fdce2cc38fc800674bd27c68a1cc24aa0d
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-09-15 12:54:29 -0500

    generated model for Users, in the model added the many-to-many with languages, cusines, music and tribe

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+	has_and_belongs_to_many :tribes
+	has_and_belongs_to_many :languages
+	has_and_belongs_to_many :cuisines
+	has_and_belongs_to_many :musics
+	
   # attr_accessible :title, :body
 end
