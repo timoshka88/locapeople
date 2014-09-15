d95f04f347dceaf51c7bf5051cf013ddeb80657d
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-09-15 16:36:58 -0500

    forgot to add the create_drinks migration on previous commit, adding it now

---
+++ db/migrate/20140915212732_create_drinks.rb
@@ -0,0 +1,14 @@
+class CreateDrinks < ActiveRecord::Migration
+  def change
+    create_table :drinks do |t|
+    	t.string 		:name
+    	t.string		:description
+    	t.string		:ingredients
+    	# t.integer		:country_id #foreign key many-to-many
+    	# t.integer		:lookup_drink_type #foreign key many-to-many
+    	# t.integer		:lookup_spirit #foreign key many-to-many
+      # t.integer		:user #foreign key many-to-many
+      t.timestamps
+    end
+  end
+end
