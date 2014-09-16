c4c3840ba3de042f65d05a13276ee6aca3a57fe9
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-09-16 16:00:13 -0500

    editing spacing

--- db/migrate/20140915212732_create_drinks.rb
+++ db/migrate/20140915212732_create_drinks.rb
@@ -1,6 +1,6 @@
 class CreateDrinks < ActiveRecord::Migration
   def change
-    create_table :drinks do |t|
+    create_table  :drinks do |t|
     	t.string 		:name
     	t.string		:description
     	t.string		:ingredients
