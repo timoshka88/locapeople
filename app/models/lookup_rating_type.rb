37275cdf057f453f45e267b8129b0110d49ce22a
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-09-16 15:53:40 -0500

    defined many-to-many between city-lookupRatingType"

--- app/models/lookup_rating_type.rb
+++ app/models/lookup_rating_type.rb
@@ -4,5 +4,6 @@ class LookupRatingType < ActiveRecord::Base
 	has_and_belongs_to_many	:events
 	has_and_belongs_to_many	:performers
 	has_and_belongs_to_many	:countries
+	has_and_belongs_to_many :cities
   # attr_accessible :title, :body
 end
