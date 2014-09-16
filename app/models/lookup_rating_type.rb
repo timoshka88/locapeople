887699a41c92ced99e0e135de80d93beff720d43
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-09-16 15:31:14 -0500

    defined many-to-many between Performer-LookupRatingType

--- app/models/lookup_rating_type.rb
+++ app/models/lookup_rating_type.rb
@@ -2,5 +2,6 @@ class LookupRatingType < ActiveRecord::Base
 	has_and_belongs_to_many :venues
 	has_and_belongs_to_many :users
 	has_and_belongs_to_many	:events
+	has_and_belongs_to_many	:performers
   # attr_accessible :title, :body
 end
