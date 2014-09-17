6918f77c12161ca1ff8a72639c18e0b110d91e22
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-09-17 15:51:39 -0500

    added column rate_amount to the country-rating-user table

--- db/migrate/20140917204807_create_join_table_lookup_countries_lookup_rating_types_users.rb
+++ db/migrate/20140917204807_create_join_table_lookup_countries_lookup_rating_types_users.rb
@@ -3,4 +3,5 @@ create_join_table :lookup_countries, :lookup_rating_types, :users do |t|
 	  t.index :lookup_country_id
 	  t.index :lookup_rating_type_id
 	  t.index :user_id
+	  t.integer :rate_amount
 end
