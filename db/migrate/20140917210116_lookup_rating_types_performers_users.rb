fb05d8594f727add600aa4589e61dd8eaf46ce44
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-09-17 16:02:51 -0500

    generated join table for performer-rating-user

---
+++ db/migrate/20140917210116_lookup_rating_types_performers_users.rb
@@ -0,0 +1,8 @@
+class LookupRatingTypesPerformersUsers < ActiveRecord::Migration
+	create_join_table :lookup_rating_types, :performers, :users do |t|
+	  t.index :lookup_rating_type_id
+	  t.index :performer_id
+	  t.index :user_id
+	  t.integer :rate_amount
+	end
+end
