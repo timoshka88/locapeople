14c5e4c3fd7c067699a17526dafff0fc45b56097
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-09-22 18:16:36 -0500

    chamged the data type of description to text and english speakers to float in the city migration

--- db/migrate/20140915210531_create_lookup_cities.rb
+++ db/migrate/20140915210531_create_lookup_cities.rb
@@ -2,8 +2,8 @@ class CreateLookupCities < ActiveRecord::Migration
   def change
     create_table      :lookup_cities do |t|
     	t.string				:name_state #includes the state if neccessary
-    	t.string				:description 
-    	t.string				:english_speakers_percentage #has to be a data which can be a float
+    	t.text   				:description 
+    	t.float  				:english_speakers_percentage #has to be a data which can be a float
     	t.belongs_to		:lookup_country #foreign key one-to-many
     	t.belongs_to    :last_call_time #foreign key one-to-many points to the hour table
 
