5a541a10bd8414073a3a791e9a05b6edab5a69fd
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-09-23 12:32:38 -0500

    changing the man-to-many relationship to has_many through, first one to go tribes-venues. created a model for the join table

--- app/models/lookup_tribe_type.rb
+++ app/models/lookup_tribe_type.rb
@@ -1,5 +1,7 @@
 class LookupTribeType < ActiveRecord::Base
-  has_and_belongs_to_many :venues
+  # has_and_belongs_to_many :venues
+  has_many :venue_tribes
+	has_many :venues, :through => :venue_tribes
 	has_and_belongs_to_many :users
 	has_and_belongs_to_many :events
 	has_and_belongs_to_many	:performers  
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,5 +1,7 @@
 class Venue < ActiveRecord::Base
-	has_and_belongs_to_many :lookup_tribe_types
+	# has_and_belongs_to_many :lookup_tribe_types
+	has_many :venue_tribes
+	has_many :lookup_tribe_types, :through => :venue_tribes
 	has_and_belongs_to_many :lookup_cuisine_types
 	has_and_belongs_to_many :lookup_music_types
 	has_and_belongs_to_many :lookup_venue_types
