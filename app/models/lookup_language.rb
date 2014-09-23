a582ea0841386943c5a6b14a1679d7590425c815
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-09-23 15:41:52 -0500

    changing the man-to-many relationship to has_many through, performer-language. created a model for the join table

--- app/models/lookup_language.rb
+++ app/models/lookup_language.rb
@@ -1,5 +1,7 @@
 class LookupLanguage < ActiveRecord::Base
-  has_and_belongs_to_many :performers
+  # has_and_belongs_to_many :performers
+  has_many :performer_languages
+	has_many :performers, :through => :performer_languages
 	has_and_belongs_to_many :users
 
   # attr_accessible :title, :body
--- app/models/performer.rb
+++ app/models/performer.rb
@@ -3,7 +3,9 @@ class Performer < ActiveRecord::Base
 	has_many :performer_tribes
 	has_many :tribes, :through => :performer_tribes
 	has_and_belongs_to_many :lookup_music_types
-	has_and_belongs_to_many :lookup_languages
+	# has_and_belongs_to_many :lookup_languages
+	has_many :performer_languages
+	has_many :lookup_languages, :through => :performer_languages
   has_and_belongs_to_many :lookup_rating_types
   belongs_to 							:user
   belongs_to    					:hometown_city, :class_name => "LookupCity"
