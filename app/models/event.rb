3609359989a2bf47fbf465a0e4b55fc34b9a1802
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-09-23 12:46:31 -0500

    changing the man-to-many relationship to has_many through, event-tribe. created a model for the join table

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ActiveRecord::Base
-	has_and_belongs_to_many :lookup_tribe_types
+	# has_and_belongs_to_many :lookup_tribe_types
+	has_many :event_tribes
+	has_many :tribes, :through => :event_tribes
 	has_and_belongs_to_many :lookup_cuisine_types
 	has_and_belongs_to_many :lookup_music_types
 	has_and_belongs_to_many :lookup_rating_types
--- app/models/lookup_tribe_type.rb
+++ app/models/lookup_tribe_type.rb
@@ -5,7 +5,9 @@ class LookupTribeType < ActiveRecord::Base
 	# has_and_belongs_to_many :users
 	has_many :user_tribes
 	has_many :users, :through => :user_tribes
-	has_and_belongs_to_many :events
+	# has_and_belongs_to_many :events
+	has_many :event_tribes
+	has_many :events, :through => :event_tribes
 	# has_and_belongs_to_many	:performers  
 	has_many :performer_tribes
 	has_many :performers, :through => :performer_tribes
