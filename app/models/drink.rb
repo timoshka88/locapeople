80dcd0d8a81f272a818c4a0acbf1275c7fd7df5a
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-09-24 17:36:17 -0500

    changing the man-to-many relationship to has_many through, drink-drink_type. created a model for the join table

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -2,7 +2,9 @@ class Drink < ActiveRecord::Base
   # has_and_belongs_to_many :countries
   has_many :drink_origins
 	has_many :countries, 		:through => :drink_origins
-  has_and_belongs_to_many :lookup_drink_types
+  # has_and_belongs_to_many :lookup_drink_types
+  has_many :drink_categorizations
+	has_many :lookup_drink_types, 		:through => :drink_categorizations
   has_and_belongs_to_many :lookup_spirits
   # has_and_belongs_to_many :users
   has_many :user_fav_drinks
---
+++ db/migrate/20140924220036_create_drink_categorizations.rb
@@ -0,0 +1,10 @@
+class CreateDrinkCategorizations < ActiveRecord::Migration
+  def change
+    create_table :drink_categorizations do |t|
+      t.integer :drink_id
+      t.integer :lookup_drink_type_id
+
+      t.timestamps
+    end
+  end
+end
