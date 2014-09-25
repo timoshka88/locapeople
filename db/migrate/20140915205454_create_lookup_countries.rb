8cf738671e6181b7284723fe15d801c67102decc
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-09-25 15:33:07 -0500

    migrated db

--- db/migrate/20140915205454_create_lookup_countries.rb
+++ db/migrate/20140915205454_create_lookup_countries.rb
@@ -14,7 +14,7 @@ class CreateLookupCountries < ActiveRecord::Migration
     	t.string	  :currency_code
     	t.integer	  :population
     	t.text		  :description
-    	t.floats	  :english_speakers_percentage #data type that accepts floats
+    	t.float	      :english_speakers_percentage #data type that accepts floats
     	t.belongs_to  :last_call_time #foreign key points to lhour and is a one-to-many
 
       t.timestamps
