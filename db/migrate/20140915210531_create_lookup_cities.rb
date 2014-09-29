dc1955a22414cdf2bee5ae476fb71ffd34076faf
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-09-29 11:30:07 -0500

    changed the name column to city_name in the LookupCities table

--- db/migrate/20140915210531_create_lookup_cities.rb
+++ db/migrate/20140915210531_create_lookup_cities.rb
@@ -1,7 +1,7 @@
 class CreateLookupCities < ActiveRecord::Migration
   def change
     create_table      :lookup_cities do |t|
-    	t.string				:name_state #includes the state if neccessary
+    	t.string				:city_name #includes the state if neccessary
     	t.text   				:description 
     	t.float  				:english_speakers_percentage #has to be a data which can be a float
     	t.belongs_to		:lookup_country #foreign key one-to-many
