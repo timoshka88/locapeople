db37c3083b11b306b73dabd350230541f4a5a443
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-09-29 12:06:16 -0500

    added a timezone column to LookupCities

--- db/migrate/20140915210531_create_lookup_cities.rb
+++ db/migrate/20140915210531_create_lookup_cities.rb
@@ -6,6 +6,7 @@ class CreateLookupCities < ActiveRecord::Migration
     	t.float  				:english_speakers_percentage #has to be a data which can be a float
       t.float         :latitude
       t.float         :longitude
+      t.string        :timezone
     	t.belongs_to		:lookup_country #foreign key one-to-many
     	t.belongs_to    :last_call_time #foreign key one-to-many points to the hour table
 
