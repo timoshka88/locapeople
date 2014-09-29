569826321bf3f6413b1b78a3b5ee6d3b0ce76346
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-09-29 14:25:20 -0500

    added a lookupRegions csv file

--- db/migrate/20140915210531_create_lookup_cities.rb
+++ db/migrate/20140915210531_create_lookup_cities.rb
@@ -1,12 +1,13 @@
 class CreateLookupCities < ActiveRecord::Migration
   def change
     create_table      :lookup_cities do |t|
-    	t.string				:city_name #includes the state if neccessary
+    	t.string				:city_name
     	t.text   				:description 
     	t.float  				:english_speakers_percentage #has to be a data which can be a float
       t.float         :latitude
       t.float         :longitude
       t.string        :timezone
+      t.string        :code
     	t.belongs_to		:lookup_country
       t.belongs_to    :lookup_region
     	t.belongs_to    :last_call_time #foreign key one-to-many points to the hour table
