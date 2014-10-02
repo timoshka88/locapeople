1033dcde8a8d9d05e2d990204d0cb5ebf911bbbf
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-10-02 15:27:09 -0500

    added the UserFavDrinkType controller, defined strong parameters, commented the attr_accessible in the model

--- app/controllers/user_cuisines_controller.rb
+++ app/controllers/user_cuisines_controller.rb
@@ -2,7 +2,7 @@ class UserCuisinesController < ApplicationController
 
 	private
 
-    def user_cuisines_params
+    def user_cuisine_params
       params.require(:user_cuisine).permit(:id, :lookup_cuisine_type_id, :user_id)
     end
 
