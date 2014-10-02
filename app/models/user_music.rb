c5e8507ae2967cb50d2837b818d8f554520127e8
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-10-02 15:32:28 -0500

    added the UserMusic controller, defined strong parameters, commented the attr_accessible in the model

--- app/models/user_music.rb
+++ app/models/user_music.rb
@@ -1,5 +1,5 @@
 class UserMusic < ActiveRecord::Base
-  attr_accessible :id, :lookup_music_type_id, :user_id
+  # attr_accessible :id, :lookup_music_type_id, :user_id
   belongs_to			:lookup_music_type
   belongs_to			:user
 end
