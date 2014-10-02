c783cc75e513c7285b3ef29563cc833c9000b25d
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-10-02 12:00:47 -0500

    added a secret_key_base in the secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 LocaPeople::Application.config.secret_token = 'a46c8eed89d702ea658902987bdabd99918b2c59edd206b7b0c99aeff615407fff996d44303d8735a11a278b668e33749720cc199cd0eeac34215c59a69f29aa'
+LocaPeople::Application.config.secret_key_base = 'b46a8ffd89d702ea658902987bdabd99918b2c59edd206b7b0c99aeff615407fff996d44303d8735a11a278b668e33749720cc199cd0eeac34215c59a69f29bb'
