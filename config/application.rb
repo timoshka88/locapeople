46f2f65b13a97ab496b85391505581c087d4af06
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-10-06 15:45:54 -0500

    modified the precompile property of the assets configuration to be able to have individual js files working on separate pages

--- config/application.rb
+++ config/application.rb
@@ -44,6 +44,8 @@ module LocaPeople
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
+    config.assets.precompile += %w( *_controller.js)
+
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
