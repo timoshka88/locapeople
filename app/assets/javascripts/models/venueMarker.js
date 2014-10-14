85205bf65f067d3f095a3625e500f8e014009c32
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-10-13 19:23:57 -0500

    added a venueMarker js file

---
+++ app/assets/javascripts/models/venueMarker.js
@@ -0,0 +1,16 @@
+function VenueMarker(){
+
+}
+
+VenueMarker.prototype = {
+	setMarkers: function(venueData){
+		var markers = []
+		var length = venueData.length
+		for (var i = 0; i< length; i++){
+			var latLng = new google.maps.LatLng(venueData[i].geometry.location[0], venueData[i].location[1]);
+			var marker = new googe.maps.Marker({position: latLng, map: this.map})
+			markers.push(marker)
+		}
+		return markers
+	}
+}
