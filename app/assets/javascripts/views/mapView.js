44a3b8d34f9e4dcdbc21a611246ad374646db1c5
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-10-13 19:30:45 -0500

    added a method to mapView for centering the map on particular coords

--- app/assets/javascripts/views/mapView.js
+++ app/assets/javascripts/views/mapView.js
@@ -5,5 +5,13 @@ function MapView(){
 MapView.prototype = {
 	drawMap: function(){
 		this.map = new google.maps.Map(document.getElementById(this.mapSelector), mapOptions);
-	}
+	},
+
+	allocateMap: function(locationCoordinates){
+		var lng = parseFloat(locationCoordinates.lng)
+		var lat = parseFloat(locationCoordinates.lat)
+		var centerMap = new google.maps.LatLng(lat, lng)
+		this.map.pantTo(centerMap)
+		this.map.setZoom(12)
+	},
 }
