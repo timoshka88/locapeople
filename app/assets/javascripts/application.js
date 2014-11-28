38492c140866c4acca791c21b1c5d7d9d3154b3d
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-11-28 16:59:03 -0500

    added in the application.js requirement to include files from ./templates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery.easing.1.3
 //= require handlebars.runtime
+//= require_tree ./templates/
 //= require_tree ./sharedJavascript/
 //= require_tree ./views/
 //= require_tree ./models/
