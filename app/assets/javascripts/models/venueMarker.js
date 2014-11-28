58d173456defaf08229c5d2c904e86d413f1d356
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-11-28 17:36:04 -0500

    commented out for now the code for index in the venue controller

--- app/assets/javascripts/models/venueMarker.js
+++ app/assets/javascripts/models/venueMarker.js
@@ -19,8 +19,10 @@ VenueMarker.prototype = {
 	createMarkersScrollingBar:function(venueData){
 		console.log("in the createMarkersScrollingBar")
 		var length = venueData.length
+		console.log(venueData)
 		for (var i = 0; i < length; i++) {
-			var photo = venueData[i].photos[0].getUrl({ 'maxWidth': 50, 'maxHeight': 50 });
+			console.log(venueData[0].photos)
+			var photo = venueData[i].photos.getUrl({ 'maxWidth': 50, 'maxHeight': 50 });
 			var venueSmallBox = HandlebarsTemplates['venues/venue_display_scroll_bar'](venueData[i])
 			$('.venues-display').append(venueSmallBox)
 				// $('.venues-display').append('<li><div class = venue-picture><img src='+ venueData[i].icon + '></div></li>')
