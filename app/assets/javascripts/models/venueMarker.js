4b6357f7505952823eec263aec05c3db4a1d807f
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-11-28 22:27:04 -0500

    in the VenueMarker got rid of the method for retreiving pictures, and deleted commented code which is not neccessary any more

--- app/assets/javascripts/models/venueMarker.js
+++ app/assets/javascripts/models/venueMarker.js
@@ -21,27 +21,14 @@ VenueMarker.prototype = {
 		var length = venueData.length
 		console.log(venueData)
 		for (var i = 0; i < length; i++) {
-			// console.log('venueData photo retrieval')
-			// console.log(venueData[0].photos)
-			// this.retrievePhoto(venueData[i])
 			var venueSmallBox = HandlebarsTemplates['venues/venue_display_scroll_bar'](venueData[i])
 			$('.venues-display').append(venueSmallBox)
-				// $('.venues-display').append('<li><div class = venue-picture><img src='+ venueData[i].icon + '></div></li>')
 		};
 	},
 
 	clearMarkerScrollingBar:function(){
 		console.log("In the clearMarker Scrolling Bar")
 		$('.venues-display').empty()
-	},
-
-	retrievePhoto: function(place){
-		var photos = place.photos
-		var photo
-		if(typeof photos !== 'undefined'){
-			photo = photos[0].getUrl({'maxWidth': 35, 'maxHeight': 35})
-		}
-		return photo
 	}
 
 }
