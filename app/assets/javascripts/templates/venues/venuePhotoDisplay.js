22be5f5d42a17e010d004f37c4c7fd982a625077
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-11-28 22:29:29 -0500

    added an && closure to the if statement in the venuePhotoDisplay, so that it checks that not only photos is undefined, but also the length of the array is greter than 0

--- app/assets/javascripts/templates/venues/venuePhotoDisplay.js
+++ app/assets/javascripts/templates/venues/venuePhotoDisplay.js
@@ -2,7 +2,7 @@ Handlebars.registerHelper("venuePhotoDisplay",function(photos, icon){
 	console.log("I'm in the venuePhoto Handlebars helper")
 	var photo 
 
-	if(typeof photos !== 'undefined'){
+	if(typeof photos !== 'undefined' && photos.length > 0){
 		return photo = photos[0].getUrl({'maxWidth': 35, 'maxHeight': 35})
 	}
 	else{
