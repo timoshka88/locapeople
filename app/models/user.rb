1728753c76dba520d6fde3394b245239b858f44e
Fatima Khalilova <fatimakhalilova@gmail.com>
2015-01-23 15:10:24 -0500

    in the user model in the self.from_omniauth added the .info to retrieve name and email from FB API

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,14 +51,13 @@ class User < ActiveRecord::Base
   	where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
   		user.provider = auth.provider
   		user.uid = auth.uid
-  		user.first_name = auth.first_name
-  		user.last_name = auth.last_name
-  		user.email = auth.email
+  		user.first_name = auth.info.first_name
+  		user.last_name = auth.info.last_name
+  		user.email = auth.info.email
   		user.oauth_token = auth.credentials.token
   		user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-  		user.save!
-  		
+  		user.save!	
   	end
-  	
   end
+
 end
