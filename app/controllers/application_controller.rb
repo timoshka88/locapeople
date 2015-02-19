0dde116f69928a13e31103d15635b51521ac3070
Fatima Khalilova <fatimakhalilova@gmail.com>
2015-02-19 17:07:11 -0500

    added a different method in the Application controller for handling the user state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,39 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
-  helper_method :user_signed_in?
-  helper_method :correct_user
+  # helper_method :current_user
+  # helper_method :user_signed_in?
+  # helper_method :correct_user
   private
 
   def current_user
-  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  	@current_user ||= User.find(id: session[:user_id]) #if session[:user_id]
   end
 
   def user_signed_in?
   	return true if current_user
   end
 
-  def correct_user?
-  	@user = User.find(parmas[:id])
-  	unless current_user == @user
-  		redirect_to root_url, :alert => "Access denied"
-  	end
+  helper_method :current_user
+  helper_method :user_signed_in?
+
+  def current_user = (user)
+    @current_user = user
+    session[:user_id] = user.nil? ? nil : user.id
   end
 
-  def authenticate_user!
-    if !current_user
-      redirect_to root_url, :alert => 'You need to sign in for access to this page.'
-    end
-  end
+  # def correct_user?
+  # 	@user = User.find(parmas[:id])
+  # 	unless current_user == @user
+  # 		redirect_to root_url, :alert => "Access denied"
+  # 	end
+  # end
+
+  # def authenticate_user!
+  #   if !current_user
+  #     redirect_to root_url, :alert => 'You need to sign in for access to this page.'
+  #   end
+  # end
 
   
 end
