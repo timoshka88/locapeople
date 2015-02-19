28e7633ef0289a6fbc73e4966c0e1b8828c76f7e
Fatima Khalilova <fatimakhalilova@gmail.com>
2015-02-19 15:11:01 -0500

    changed the method create/destroy in the sessionsController. Now the methods when a user logs in check if the user already exists and has an authorization, if not than creates the user or adds and authorization

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,20 @@
 class SessionsController < ApplicationController
 	def create
-		user = User.from_omniauth(env['omniauth.auth'])
-		session[:user_id] = user.id 
-		redirect_to user_path(user.id)
+		auth = request.env(['omniauth.auth'])
+		unless @auth = Authorization.find_from_hash(auth)
+			@auth = Authorization.create_from_hash(auth, current_user)
+		end
+
+		self.current_user = @auth.user
+		render :text => "Welcome, #{current_user.first_name}"
+		# user = User.from_omniauth(env['omniauth.auth'])
+		# session[:user_id] = user.id 
+		# redirect_to user_path(user.id)
 	end
 
 	def destroy
-		session[:user_id] = nil
+		self.current_user = nil
+		# session[:user_id] = nil
 		redirect_to root_url
 	end
 
