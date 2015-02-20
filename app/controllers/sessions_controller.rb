f817dfefd7b5ea7816f95f14664f2bff2d25faa5
Fatima Khalilova <fatimakhalilova@gmail.com>
2015-02-19 22:30:22 -0500

    returned on the Landing page the if/else statement when user is logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,20 +25,20 @@ class SessionsController < ApplicationController
       # account. But we found the identity and the user associated with it 
       # is the current user. So the identity is already associated with 
       # this user. So let's display an error message.
-      redirect_to root_url, notice: "Already linked that account!"
+      redirect_to user_path(current_user.id), notice: "Already linked that account!"
     else
       # The identity is not associated with the current_user so lets 
       # associate the identity
       @identity.user = current_user
       @identity.save()
-      redirect_to root_url, notice: "Successfully linked that account!"
+      redirect_to user_path(current_user.id), notice: "Successfully linked that account!"
     end
   else
     if @identity.user.present?
       # The identity we found had a user associated with it so let's 
       # just log them in here
       self.current_user = @identity.user
-      redirect_to root_url, notice: "Signed in!"
+      redirect_to user_path(current_user.id), notice: "Signed in!"
     else
       # No user associated with the identity so we need to create a new one
       redirect_to new_user_url, notice: "Please finish registering"
